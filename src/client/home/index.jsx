d0cf4c5143cbdbcc91faf5bffdf6972f925b963d
Carlos Santana <carlos.x.santana.-nd@disney.com>
2018-08-12 17:10:08 -0700

    Updating home component

--- src/client/home/index.jsx
+++ src/client/home/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Home.scss';
 
 const Home = props => (
-  <h1 className={styles.Home}>Hello {props.name || 'World'} (Jenkins)</h1>
+  <h1 className={styles.Home}>Hello {props.name || 'World'} (Jenkins - from Pull Request)</h1>
 );
 
 export default Home;
