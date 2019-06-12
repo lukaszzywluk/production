b4ec4abd8a20acccdc6f574767cfb936225a5e3e
Lukasz Zywluk <lzywluk@casio.com>
2019-06-12 14:37:01 -0400

    Refactor code

--- src/client/home/index.jsx
+++ src/client/home/index.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import styles from "./Home.scss";
 
 const Home = props => (
-  <>
+  <ReactFragment>
     <h1 className={styles.Home}>Hello {props.name || "World"} (Jenkins)</h1>
     <h2>This is a test!</h2>
-  </>
+  </ReactFragment>
 );
 
 export default Home;
