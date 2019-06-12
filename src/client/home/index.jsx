8b3a66ad82265f6d3f816a2607299b9a3506b636
Lukasz Zywluk <lzywluk@casio.com>
2019-06-12 14:28:45 -0400

    Wrap tags in react fragment

--- src/client/home/index.jsx
+++ src/client/home/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import styles from './Home.scss';
+import React from "react";
+import styles from "./Home.scss";
 
 const Home = props => (
-  <h1 className={styles.Home}>Hello {props.name || 'World'} (Jenkins)</h1>
-  <h2>This is a test!</h2>
+  <>
+    <h1 className={styles.Home}>Hello {props.name || "World"} (Jenkins)</h1>
+    <h2>This is a test!</h2>
+  </>
 );
 
 export default Home;
