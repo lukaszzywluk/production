5ba37f11f189d9bac4555c8f09811afe2c9ec5b4
Lukasz Zywluk <lzywluk@casio.com>
2019-06-17 11:02:35 -0400

    Import React.Fragment

--- src/client/home/index.jsx
+++ src/client/home/index.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import { Fragment, default as React } from "react";
 import styles from "./Home.scss";
 
 const Home = props => (
-  <ReactFragment>
+  <Fragment>
     <h1 className={styles.Home}>Hello {props.name || "World"} (Jenkins)</h1>
     <h2>This is a test!</h2>
-  </ReactFragment>
+  </Fragment>
 );
 
 export default Home;
